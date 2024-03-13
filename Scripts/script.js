98a8d8803166a212d4e276cda2c89878f1208697
Kalana Bamunuarachchi <kalana.bamunuarachchi@outlook.com>
2024-03-13 14:51:59 +0530

    structure

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,9 @@
+//preloader function
 const preloader = document.querySelector('.preloader');
 const dashboard = document.querySelector('.dashboard');
 
 setTimeout(() => {
     preloader.style.display = 'none';
     dashboard.style.display = 'block';
-}, 3000); // Replace with desired preloader duration (ms)
+}, 4000); // Replace with desired preloader duration (ms)
+
