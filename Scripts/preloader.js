3c57a6dd75aa9b19dd18964c86c33c979b904196
Kalana Bamunuarachchi <kalana.bamunuarachchi@outlook.com>
2024-03-14 01:03:57 +0530

    additional js and css changes

---
+++ Scripts/preloader.js
@@ -0,0 +1,7 @@
+const preloader = document.querySelector('.preloader');
+const dashboard = document.querySelector('.dashboard');
+
+setTimeout(() => {
+  preloader.style.display = 'none';
+  dashboard.style.display = 'block';
+}, 4000); // Replace with desired preloader duration (ms)
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,21 @@
-//preloader function
-const preloader = document.querySelector('.preloader');
-const dashboard = document.querySelector('.dashboard');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDznI1dGnjCacKwd_IKGtNG0I2eRttYXCI",
+  authDomain: "hevea-7a027.firebaseapp.com",
+  projectId: "hevea-7a027",
+  storageBucket: "hevea-7a027.appspot.com",
+  messagingSenderId: "741000065624",
+  appId: "1:741000065624:web:04200b7af488c79bd6902d",
+  measurementId: "G-NBXXKCHSSP"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-setTimeout(() => {
-    preloader.style.display = 'none';
-    dashboard.style.display = 'block';
-}, 4000); // Replace with desired preloader duration (ms)
 
